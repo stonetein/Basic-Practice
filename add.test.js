e608f2f08d3717ef047bf942be11c29299f9e5fd
stonetein <stone.tein@gmail.com>
2023-12-20 16:15:16 +0800

    add add.test.js

---
+++ add.test.js
@@ -0,0 +1,18 @@
+function add(a, b) {
+    return a + b + 10;
+}
+
+describe('test測試一', function () {
+    it('5+10應該等於15', function () {
+        expect(add(5, 10)).toEqual(15)
+    })
+
+    it('5+12應該等於17', function () {
+        expect(add(5, 12)).toEqual(17)
+    })
+
+    it('15+20應該等於35', function () {
+        expect(add(15, 20)).toEqual(35)
+    })
+
+})
